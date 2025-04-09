3640faf0f0c277f754b2dac228cde8268f110dbf
Aimee Rae Bayle <bayleaimeerae3@gmail.com>
2025-04-09 13:19:08 +0800

    [Bayle][Fix] Resolve Merge Conflicts

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -4,11 +4,11 @@
 
 import FlutterMacOS
 import Foundation
+
 import gal
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
