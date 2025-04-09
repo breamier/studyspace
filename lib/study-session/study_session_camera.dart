b609564a058250c227c1635ed547605850075892
Piage <piageaguirre@gmail.com>
2025-04-09 11:17:07 +0800

    Text on study session camera screen

--- lib/study-session/study_session_camera.dart
+++ lib/study-session/study_session_camera.dart
@@ -1,5 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:gal/gal.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StudySessionCamera extends StatefulWidget {
   const StudySessionCamera({super.key});
@@ -24,6 +26,18 @@ class _StudySessionCameraState extends State<StudySessionCamera> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _buildUI(),
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_circle_left_outlined,
+              color: Colors.white,
+            )),
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.black,
     );
   }
 
@@ -34,7 +48,76 @@ class _StudySessionCameraState extends State<StudySessionCamera> {
         child: CircularProgressIndicator(),
       );
     }
-    return SafeArea(child: SizedBox.expand());
+    return SafeArea(
+        child: SizedBox.expand(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("session study goal",
+                  style: GoogleFonts.arimo(
+                    color: Colors.white,
+                    fontSize: 14,
+                  )),
+              Text("Statistics and Probability",
+                  style: GoogleFonts.arimo(
+                    color: Colors.white,
+                    fontSize: 20,
+                  )),
+            ],
+          ),
+          Spacer(),
+          Text("Probing for life...",
+              style: GoogleFonts.arimo(
+                color: Colors.white,
+                fontSize: 14,
+              )),
+          Text("Take a picture for this study session!",
+              style: GoogleFonts.arimo(
+                color: Colors.white,
+                fontSize: 14,
+              )),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.50,
+            width: MediaQuery.sizeOf(context).width * 0.80,
+            child: CameraPreview(cameraController!),
+          ),
+          IconButton(
+              onPressed: () async {
+                XFile picture = await cameraController!.takePicture();
+                Gal.putImage(picture.path);
+              },
+              iconSize: 100,
+              icon: const Icon(
+                Icons.camera,
+                color: Colors.deepPurple,
+                size: 100,
+              )),
+          Spacer(),
+          Column(
+            children: [
+              TextButton(
+                  onPressed: () {},
+                  child: Text("Can't take a photo now. Skip",
+                      style: GoogleFonts.arimo(
+                          color: Colors.white,
+                          fontSize: 20,
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.white))),
+              Text("This will damage astronaut's health",
+                  style: GoogleFonts.arimo(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ))
+            ],
+          ),
+          Spacer()
+        ],
+      ),
+    ));
   }
 
   Future<void> _SetupCameraController() async {
@@ -46,9 +129,7 @@ class _StudySessionCameraState extends State<StudySessionCamera> {
             CameraController(_cameras.first, ResolutionPreset.high);
       });
       cameraController?.initialize().then((_) {
-        setState(() {
-
-        });
+        setState(() {});
       });
     }
   }
