6fc8fc72a61d2cf47d129768dcc33f2efb14935d
Piage <81365404+RedRogue10@users.noreply.github.com>
2025-05-20 20:15:47 +0800

    Final fix promise

--- lib/study-session/study_session.dart
+++ lib/study-session/study_session.dart
@@ -258,7 +258,7 @@ class _StateStudySession extends State<StudySession> {
         time++;
       });
     }
-    if (dontShowCheckIns && time % 3600 == 0 && !showPopUp && isActive) {
+    if (!dontShowCheckIns && time % 3600 == 0 && !showPopUp && isActive) {
       setState(() {
         isActive = false;
         promptIndex = Random().nextInt(3);
